58dc6e7118bf86a17cf188cc39ba7dac8d5b5268
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-03-08 18:04:05 +0300

    next step

--- js/script-a6.js
+++ js/script-a6.js
@@ -1,28 +1,26 @@
-  
+const buttons = [7,3,1]
 
 function addProgress(evetn) {
   $("#progressbar").each(function() {
-  
-    let procent = evetn.target.data("progress");
-  
-
+    let procent = $(evetn.target).data("progress");
     let current_progress = procent + parseInt($(this).attr("aria-valuenow"));
-
-
-
     $(this)
       .attr("aria-valuenow",current_progress)
       .css("width", current_progress + "%")
       .text(current_progress+"%");
 
+    $(".btn").each(function(){
+      $( this ).attr('disabled','disabled');
+    })
   }); 
- 
 }
-
   
 function init() {
-  $("#btn-1").click(addProgress);
-  console.log("скрипт подгрузился");
+  buttons.forEach(Item => {
+    const $button = $(`<button type="button" class="btn btn-dark ml-3" data-progress="${Item}">+${Item}%</button>`);
+    $button.insertAfter( $(".progress") );
+  });
+  $(".btn").click(addProgress);
 }
 
  $(document).ready(init); 
